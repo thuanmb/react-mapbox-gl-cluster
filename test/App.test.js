4333c3f4e199222d38b547c534ea9252d3b15b50
Thuan Bui <bui@niometrics.com>
2018-04-19 15:05:23 +0700

    fix test failed

--- test/App.test.js
+++ test/App.test.js
@@ -2,8 +2,8 @@ jest.mock("mapbox-gl");
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { ReactMapboxGlCluster } from "./modules";
-import { data } from "./data";
+import { ReactMapboxGlCluster } from "../src/modules";
+import { data } from "../src/data";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
